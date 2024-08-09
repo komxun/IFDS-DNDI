fc6e2820ac6057a0280ab3d9bf141b4f2fd02ea8
Komsun Tamanakijprasart <K.Tamanakijprasart.263@cranfield.ac.uk>
2024-08-09 15:01:26 +0100

    updated MyPlot to have option

--- MyPlot.m
+++ MyPlot.m
@@ -1,9 +1,13 @@
-function MyPlot(swarm, tRange, comsHistory, zoomIn)
+function MyPlot(swarm, tRange, comsHistory, option)
+    zoomIn = option.zoomIn;
+    wantLegend = option.legend;
+
     syms XX YY ZZ Gamma(XX,YY,ZZ) Gamma_star(XX,YY,ZZ)
     colorList = ["#A2142F", "#4DBEEE", "#77AC30", "#7E2F8E","#EDB120"];
+    agentPlot = [];
     for rt = tRange
         % ======== Plot leader UAV =======================
-        plot3(swarm{1}.trajectory(1,rt), swarm{1}.trajectory(2,rt), swarm{1}.trajectory(3,rt), 'ok-', 'MarkerSize', 10, 'LineWidth', 1.5)
+        agentPlot(1) = plot3(swarm{1}.trajectory(1,rt), swarm{1}.trajectory(2,rt), swarm{1}.trajectory(3,rt), 'ok-', 'MarkerSize', 10, 'LineWidth', 1.5);
         hold on, grid on
         % ======== Plot leader trajectory ================
         plot3(swarm{1}.trajectory(1,1:rt), swarm{1}.trajectory(2,1:rt), swarm{1}.trajectory(3,1:rt), 'k-','LineWidth', 1.5)
@@ -20,12 +24,12 @@ function MyPlot(swarm, tRange, comsHistory, zoomIn)
         for j = 2:length(swarm)
             
             % ======== Plot follower UAV =======================
-            plot3(swarm{j}.trajectory(1,rt), swarm{j}.trajectory(2,rt), swarm{j}.trajectory(3,rt), '>' ,'LineWidth', 1.5, 'Color', colorList(j-1))
+            agentPlot(j) = plot3(swarm{j}.trajectory(1,rt), swarm{j}.trajectory(2,rt), swarm{j}.trajectory(3,rt), '->' ,'LineWidth', 1.5, 'Color', colorList(j-1));
             % ======== Plot follower's trajectory =======================
             plot3(swarm{j}.trajectory(1,1:rt), swarm{j}.trajectory(2,1:rt), swarm{j}.trajectory(3,1:rt), '-','LineWidth', 1.5, 'Color', colorList(j-1))
             destin = swarm{j}.GetItsDestination();
             if (rt<=length(swarm{j}.DA.Paths)) && ~isempty(swarm{j}.DA.Paths{rt})
-                plot3(swarm{j}.DA.Paths{rt}(1,:), swarm{j}.DA.Paths{rt}(2,:), swarm{j}.DA.Paths{rt}(3,:), '--' , 'LineWidth', 1.2, 'Color', colorList(j-1))
+                plot3(swarm{j}.DA.Paths{rt}(1,:), swarm{j}.DA.Paths{rt}(2,:), swarm{j}.DA.Paths{rt}(3,:), '--' , 'LineWidth', 2, 'Color', colorList(j-1))
                 scatter3(destin(1), destin(2), destin(3),'xr', 'sizedata', 150, 'LineWidth', 1.5)
             end
             
@@ -54,7 +58,10 @@ function MyPlot(swarm, tRange, comsHistory, zoomIn)
         end
 
     end
-
+    if wantLegend
+        legend(agentPlot, "Leader", "Agent1", "Agent2", "Agent3", "Agent4", "Agent5", 'Orientation', 'horizontal',...
+            'Location', [0.12507287289916 0.0723520069288918 0.448046875 0.0356394129979036])
+    end
 end
 
 function [Gamma, Gamma_star] = PlotObject(Object, Rg, i, N, X, Y, Z, Gamma, Gamma_star)
